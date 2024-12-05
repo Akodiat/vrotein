6677f1fb4c3e30cf87a787ed30da7de22a8c213f
Joakim Bohlin <joakim.bohlin@chalmers.se>
2024-12-05 10:24:38 +0100

    Add VR libraries

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import * as THREE from "three";
 import { TrackballControls } from "three/addons/controls/TrackballControls.js";
+import { VRButton } from "three/addons/webxr/VRButton.js";
+import { XRControllerModelFactory } from "three/addons/webxr/XRControllerModelFactory.js";
+import { XRHandModelFactory } from "three/addons/webxr/XRHandModelFactory.js";
 import * as CANNON from "../lib/cannon-es.js";
 import { loadLocal } from "./file.js";
 
@@ -22,8 +25,18 @@ class PhysicalMesh {
 }
 
 class PhysicalSphere extends PhysicalMesh {
-    constructor(radius, material, mass = 0, position = new THREE.Vector3(), segments=8) {
-        const sphereGeometry = new THREE.SphereGeometry(radius, segments, segments);
+    constructor(
+        radius,
+        material,
+        mass = 0,
+        position = new THREE.Vector3(),
+        segments = 8,
+    ) {
+        const sphereGeometry = new THREE.SphereGeometry(
+            radius,
+            segments,
+            segments,
+        );
         const mesh = new THREE.Mesh(sphereGeometry, material);
 
         const shape = new CANNON.Sphere(radius);
@@ -38,6 +51,10 @@ class PhysicalSphere extends PhysicalMesh {
 
 // three.js variables
 let camera, scene, renderer, controls;
+let container;
+let hand1, hand2;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
 
 // cannon.js variables
 let world;
@@ -53,10 +70,10 @@ function init() {
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
-        0.1,
-        1000,
+        0.01,
+        100,
     );
-    camera.position.z = 5;
+    camera.position.z = 1;
 
     // Scene
     scene = new THREE.Scene();
@@ -65,8 +82,63 @@ function init() {
     renderer = new THREE.WebGLRenderer({
         antialias: true,
     });
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.shadowMap.enabled = true;
+    renderer.xr.enabled = true;
     document.body.appendChild(renderer.domElement);
+
+    const sessionInit = {
+        requiredFeatures: ["hand-tracking"],
+    };
+
+    document.body.appendChild(VRButton.createButton(renderer, sessionInit));
+
+    // controllers
+    controller1 = renderer.xr.getController(0);
+    scene.add(controller1);
+
+    controller2 = renderer.xr.getController(1);
+    scene.add(controller2);
+
+    const controllerModelFactory = new XRControllerModelFactory();
+    const handModelFactory = new XRHandModelFactory();
+
+    // Hand 1
+    controllerGrip1 = renderer.xr.getControllerGrip(0);
+    controllerGrip1.add(
+        controllerModelFactory.createControllerModel(controllerGrip1),
+    );
+    scene.add(controllerGrip1);
+
+    hand1 = renderer.xr.getHand(0);
+    hand1.add(handModelFactory.createHandModel(hand1));
+
+    scene.add(hand1);
+
+    // Hand 2
+    controllerGrip2 = renderer.xr.getControllerGrip(1);
+    controllerGrip2.add(
+        controllerModelFactory.createControllerModel(controllerGrip2),
+    );
+    scene.add(controllerGrip2);
+
+    hand2 = renderer.xr.getHand(1);
+    hand2.add(handModelFactory.createHandModel(hand2));
+    scene.add(hand2);
+
+    const geometry = new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -1),
+    ]);
+
+    const line = new THREE.Line(geometry);
+    line.name = "line";
+    line.scale.z = 5;
+
+    controller1.add(line.clone());
+    controller2.add(line.clone());
 
     // Lights
     let hemilight = new THREE.HemisphereLight(0x808080, 0x606060);
@@ -89,19 +161,20 @@ function init() {
         color: 0xffffff,
     });
 
+    container = new THREE.Group();
+    container.scale.divideScalar(10)
+    scene.add(container);
+
     loadLocal("resources/8p1a.oxview").then((systems) => {
         aminoAcids = [];
         for (const system of systems) {
             for (const strand of system.strands) {
                 for (const e of strand.monomers) {
                     const sphere = new PhysicalSphere(
-                        0.25,
-                        material,
-                        1,
-                        e.position,
+                        0.25, material, 1, e.position
                     );
                     sphere.strandId = strand.id; // TODO: save in new class
-                    scene.add(sphere.mesh);
+                    container.add(sphere.mesh);
                     world.addBody(sphere.body);
                     aminoAcids.push(sphere);
                 }
@@ -126,7 +199,7 @@ function init() {
                     localAnchorB: new CANNON.Vec3(0, 0, 0),
                     restLength: dist,
                     stiffness: 10,
-                    damping: 0.0,
+                    damping: 0.0
                 });
 
                 // Compute the force after each step
@@ -135,14 +208,13 @@ function init() {
                 });
             }
         }
-
     });
 
     const redMaterial = new THREE.MeshStandardMaterial({
         color: 0xff0000,
     });
     repulsiveSphere = new PhysicalSphere(1, redMaterial, 1, new THREE.Vector3());
-    scene.add(repulsiveSphere.mesh);
+    container.add(repulsiveSphere.mesh);
     world.addBody(repulsiveSphere.body);
 }
 
@@ -153,7 +225,7 @@ function onWindowResize() {
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+    renderer.setAnimationLoop(animate);
 
     // Step the physics world
     world.fixedStep();
@@ -167,8 +239,8 @@ function animate() {
     com.divideScalar(aminoAcids.length);
 
     repulsiveSphere.body.position.copy(com);
-    repulsiveSphere.body.position.x += Math.sin(performance.now()/2000) * 5;
-    repulsiveSphere.update()
+    repulsiveSphere.body.position.x += Math.sin(performance.now() / 2000) * 5;
+    repulsiveSphere.update();
 
     controls.update();
 
